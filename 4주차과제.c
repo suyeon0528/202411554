72649a3aee6a91ca7760c15e4ce1b18ae79dd6a2
suyeon0528 <sooyun050528@naver.com>
2024-09-28 17:59:45 +0900

    Add files via upload

---
+++ 4주차과제.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int i, j, s;
+	for (i = 1; i <= s; i++) {
+		for (s = 1; s <= s - i; s++) {
+			printf(" ");
+		}
+		for (j = 1; j <= 2 * i - 1; j++) {
+			printf("*");
+		}
+		printf("\n");
+
+	return 0;
+
+}
